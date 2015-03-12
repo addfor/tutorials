f376decdf903f0dacd27ce60b6c9a240a2d27362
Sebastiano Barrera <sebastiano.barrera@gmail.com>
2015-03-12 16:38:28 +0100

    Pulled Bokeh-based `plot_utilities.py'
    Had forgotten to copy `numpy/utilities/plot_utilities.py' from the old repo

--- numpy/utilities/plot_utilities.py
+++ numpy/utilities/plot_utilities.py
@@ -1,12 +1,12 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import bokeh.plotting as bk
 
 def plot_01():
     image = np.random.randn(120, 120)
-    plt.imshow(image, cmap=plt.cm.hot)    
-    plt.colorbar()
-    plt.show()
+    fig = bk.figure()
+    fig.image([image], x=[0], y=[0], dw=[1], dh=[1], palette='OrRd9')
+    return fig
 
 if __name__ == '__main__':
-    plot_01()
-    plt.show()
+    fig = plot_01()
+    bk.show(fig)
