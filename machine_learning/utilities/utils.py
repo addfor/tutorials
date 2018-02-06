921f9349e24316178ad44403f1014dbe0f94b669
Matteo Zola <matteo.zola@gmail.com>
2018-02-06 22:45:24 +0100

    Add MLP class to notebook, and lazy_property to utils

--- machine_learning/utilities/utils.py
+++ machine_learning/utilities/utils.py
@@ -1,3 +1,15 @@
+def lazy_property(function):
+    attribute = '_lazy_' + function.__name__
+    
+    @property
+    @functools.wraps(function)
+    def wrapper(self):
+        if not hasattr(self, attribute):
+            setattr(self, attribute, function(self))
+        return getattr(self, attribute)
+    return wrapper
+
+
 """
 (Copied from OpenDeep https://github.com/vitruvianscience/opendeep)
 
